eca51f3a3a31f77b480dcdccaf6f4ad67cc6df89
deepaknagar62 <nagardeepak9098@gmail.com>
2023-10-26 19:20:14 +0530

    new code

--- react-settingapp/Server.js
+++ react-settingapp/Server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(bodyParser.json());
 app.use(cors());
